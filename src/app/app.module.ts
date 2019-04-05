d5ed9b98b5121ea4226f4bfabb7fc19ae583242f
Lucas <lucas.pantchenko@orange.fr>
2019-04-05 11:33:33 +0200

    card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ApiModule } from './rest';
+import { HttpClientModule } from '@angular/common/http';
+import { MComponent } from './m/m.component';
+import { ProductModule } from './product/product.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ApiModule,
+    HttpClientModule,
+    ProductModule
   ],
   providers: [],
   bootstrap: [AppComponent]
